1fe0da4b7529215631674a403eb52d30ac39269e
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-08-23 14:23:09 -0400

    restore init - woops

--- mpl_point_clicker/__init__.py
+++ mpl_point_clicker/__init__.py
@@ -2,3 +2,11 @@
 
 # Copyright (c) Ian Hunt-Isaak.
 # Distributed under the terms of the Modified BSD License.
+
+from ._version import __version__
+from ._clicker import clicker
+
+__all__ = [
+    "__version__",
+    "clicker",
+]
